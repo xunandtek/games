e068b08df884e4b4f0ab16eb96e6ea0227f59cef
xunandtek <72821027+xunandtek@users.noreply.github.com>
2023-05-09 12:26:36 -0700

    initial setup

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Database {
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+
+    function getConnection($this->host){
+
+    }
+}
+
+?>
---
+++ notes.php
@@ -0,0 +1,24 @@
+<!-- 
+
+version 1 
+
+required form fields
+
+name of the game
+name of the game publisher
+rating of the game
+number of players the game supports
+
+function
+
+add, edit, delete games
+
+search games
+
+i can utilize datatables for the add edit and delete function
+but what if they want a logo and image ? 
+
+
+
+
+ -->
