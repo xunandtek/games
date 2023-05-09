35e901ebdc55f7d8d0648892ea6c9e6c8befe852
xunandtek <72821027+xunandtek@users.noreply.github.com>
2023-05-09 12:44:49 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Games</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<form action="POST">
+<input type="text" name="gameName" placeholder="Title">
+<input type="text" name="publisher" placeholder="Publisher">
+<input type="text" name="rating" placeholder="Rating">
+<input type="text" name="players" placeholder="\# of Players">
+<input type="submit" name="submit">
+</form>
+</body>
+<html>
